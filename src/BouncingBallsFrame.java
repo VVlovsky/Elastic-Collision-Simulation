1d4dc74f9acf2ff0f4dc8bdf776a0cb124bdafcb
vvlovsky <slvtrshkv@gmail.com>
2020-03-28 17:02:06 +0100

    added src files

---
+++ src/BouncingBallsFrame.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BouncingBallsFrame extends JFrame {
+
+    BouncingBallsPanel bbPanel = new BouncingBallsPanel();
+    static public int screenHeight = 1080;
+    static public int screenWidth = 1920;
+    Color BACKGROUNDCOLOR = new Color(252, 232, 255);
+
+
+    BouncingBallsFrame() {
+        super("Bouncing balls");
+        buildGui();
+        bbPanel.setBackground(BACKGROUNDCOLOR);
+    }
+
+    void buildGui() {
+        JPanel root = new JPanel();
+        root.setLayout(new BorderLayout());
+        JPanel northPanel = new JPanel();
+
+        JButton start = new JButton("Start");
+        start.addActionListener(p -> bbPanel.onStart());
+        northPanel.add(start);
+
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(p -> bbPanel.onStop());
+        stop.setEnabled(false);
+        northPanel.add(stop);
+
+        start.addActionListener(p -> {
+            start.setEnabled(false);
+            stop.setEnabled(true);
+        });
+        stop.addActionListener(p -> {
+            stop.setEnabled(false);
+            start.setEnabled(true);
+        });
+
+        JButton plus = new JButton("Plus");
+        plus.addActionListener(p -> bbPanel.onPlus());
+        northPanel.add(plus);
+
+        JButton minus = new JButton("Minus");
+        minus.addActionListener(p -> bbPanel.onMinus());
+        northPanel.add(minus);
+
+        JButton add100 = new JButton("Add 100");
+        add100.addActionListener(p->bbPanel.onPlus100());
+        northPanel.add(add100);
+        root.add(northPanel, BorderLayout.NORTH);
+
+        root.add(bbPanel, BorderLayout.CENTER);
+        setContentPane(root);
+    }
+
+    public static void main(String[] args) {
+        BouncingBallsFrame frame = new BouncingBallsFrame();
+
+        frame.setSize(screenWidth, screenHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+
+    }
+}
